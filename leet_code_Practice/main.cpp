1ee004e69b56529b848b8f165fbae8462a0b89f3
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2022-02-20 14:32:27 -0800

    Initial Commit

---
+++ leet_code_Practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leet_code_Practice
+//
+//  Created by Brandon Johns on 2/20/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
