3ab0e6b8167e008514e426daadd3f4bc37d11fb1
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2022-07-29 15:20:56 -0700

    comments

--- leet_code_Practice/Problems_easy/longest_common_prefix.cpp
+++ leet_code_Practice/Problems_easy/longest_common_prefix.cpp
@@ -13,18 +13,23 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        
-    
+        string answer = "";
+    // vector find dize
     int size = strs.size();
+    // size is zero return nothing
     if (size ==0)
         return "";
-    string answer = "";
+    // storting string
     sort(begin(strs), end(strs));
+        // set first place in the string
     string first = strs[0];
+        // based on size select next position
     string second = strs[size-1];
     
+        //loop through the string
     for (int i = 0; i<first.size(); i++)
     {
+        // first and second position are equal add them together 
         if(first[i]==second[i])
         {
             answer = answer +first[i];
